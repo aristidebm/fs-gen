55fe0f47dff36c899c6a8e78924e3c19f6315e4b
Aristide <aristide.bamazi@imsp-uac.org>
2022-02-02 22:01:30 +0000

    add utils functions skeleton

---
+++ utils.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+def isfile(path: str) -> bool:
+    pass
+
+
+def isdir(path: str) -> bool:
+    pass
+
+
+def islink(path: str) -> bool:
+    pass
+
+
+def rm(dirname: Path) -> None:
+    pass
+
+
+def touch(path: Path) -> None:
+    pass
+
+
+def exists(path: Path) -> None:
+    pass
