412dee4c052cd02f4f693ea3a767979cd8b14d7f
Aristide <aristide.bamazi@imsp-uac.org>
2022-02-02 22:24:31 +0000

    fill utils functions.

--- utils.py
+++ utils.py
@@ -1,25 +1,27 @@
 from pathlib import Path
 
 
-def isfile(path: str) -> bool:
-    pass
+def isfile(path: str, del="/") -> bool:
+    return not path.endswith(f"{del}")
 
+def isdir(path: str, del="/") -> bool:
+    return path.endswith(f"{del}")
 
-def isdir(path: str) -> bool:
-    pass
-
+def exists(path: Path) -> bool:
+    return path.exists()
 
 def islink(path: str) -> bool:
     pass
 
 
-def rm(dirname: Path) -> None:
-    pass
+def rm(filename: Path) -> None:
+    # FIXME: find the diff between this and shutil.rmtree
+    if filename.is_dir():
+        Path.rmdir(filename)
+        return
+    filename.unlink()
 
 
 def touch(path: Path) -> None:
-    pass
+    path.touch()
 
-
-def exists(path: Path) -> None:
-    pass
