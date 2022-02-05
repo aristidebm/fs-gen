4d62c47c82bc4ae63005808371395974db68f4ec
Aristide <aristide.bamazi@imsp-uac.org>
2022-02-05 19:26:34 +0000

    fix parsing logic (wip)

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import os
+import re
+import sys
 import logging
 import typing
 from collections import namedtuple
+from pathlib import Path
 
-from .utils import BaseParsingException, ValidationError, exists, absolute, isempty
+from utils import (
+    BaseParsingException,
+    ValidationError,
+    exists,
+    absolute,
+    isempty,
+    isdir,
+    isfile,
+    touch,
+)
 
 Option = namedtuple("option", ["filename", "outdir", "delimiter", "indent"])
 
 
 def main():
-    FileParser(logger_level=logging.DEBUG).parse()
+    filename = sys.argv[1]
+    FileParser(logger_level=logging.DEBUG).parse(filename)
 
 
 class FileParser:
@@ -16,16 +30,17 @@ class FileParser:
         self._logger = logging.getLogger(self.name)
         self._logger_level = logger_level
         self._config_logger()
-        self._cursor_position = 1
+        self._previous_position = 0
         self._current_dir = None
         self._base_dir = None
-        self._line_number
+        self._line_number = None
 
     def parse(
+        self,
         filename: typing.Union[str, Path],
-        outdir: typing.Union[str, Path] = None,
-        delimiter: str = None,
-        indent: str = None,
+        outdir: typing.Union[str, Path] = ".",
+        delimiter: str = "/",
+        indent: str = "\t",
     ):
         """
         filename: The path to file to parse.
@@ -33,9 +48,13 @@ class FileParser:
         delimiter: The folder marker sign [default = /]
         indent: The indentation marker sign [default = \t]
         """
-        options = Option(
-            filename=filename, outdir=outdir, delimiter=delimiter, indent=indent
-        )
+        options = {
+            "filename": filename,
+            "outdir": outdir,
+            "delimiter": delimiter,
+            "indent": indent,
+        }
+
         try:
             options = self._validate(options)
         except ValidationError as e:
@@ -48,12 +67,11 @@ class FileParser:
             filename=filename, outdir=outdir, delimiter=delimiter, indent=indent
         )
 
-    def _parse(filename: Path, outdir: Path, delimiter: str, indent: str):
+    def _parse(self, filename: Path, outdir: Path, delimiter: str, indent: str):
         with open(filename, "r") as f:
             self._logger.info("Starting ...")
-            hierarchy = f.read()
-            root = next(hierarchy)
 
+            root = next(f).strip("\n")
             if not isdir(root, delimiter=delimiter):
                 self._logger.error("The root of hierarchy must be a folder.")
                 return
@@ -64,44 +82,68 @@ class FileParser:
             self._line_number = 1
             self._current_dir = self._base_dir
 
-            for line in hierarchy:
+            for line in f:
                 self._line_number += 1
+                line = line.strip("\n")
                 parent_dir = self._compute_parent_dir(line, indent)
                 if not parent_dir:
                     continue
 
                 if isfile(line):
-                    touch(self.parent_dir / line)
+                    touch(parent_dir / self._clean_line(line, delimiter, indent))
 
                 if isdir(line):
-                    (self.parent_dir / line).mkdir()
+                    parent_dir /= self._clean_line(line, delimiter, indent)
+                    parent_dir.mkdir()
+                    self._current_dir = parent_dir
+                    self._previous_position += 1
 
-    def _compute_base_dir(outdir: Path, root: Path):
-        root = root if root.is_absolute() else absolute(root)
-        outdir = outdir if outdir.is_absolute() else absolute(outdir)
-        return outdir if outdir == root else outdir
+    def _compute_base_dir(
+        self, outdir: typing.Union[str, Path], root: typing.Union[str, Path]
+    ):
+        root = (
+            root
+            if isinstance(root, Path) and root.is_absolute()
+            else absolute(Path(root))
+        )
+        outdir = (
+            outdir
+            if isinstance(root, Path) and outdir.is_absolute()
+            else absolute(Path(outdir))
+        )
 
-    def _compute_parent_dir(line: str, indent: str):
+        if root != outdir and root.parent != outdir:
+            self._logger.warning(
+                "The root directory have to be child of outdir. outdir is took as fallback."
+            )
+            root = outdir
+
+        return outdir if outdir == root else root
+
+    def _compute_parent_dir(self, line: str, indent: str):
         pos = self._compute_cursor_position(line, indent)
-        # ignore files that are more step greater than the previous
-        # since the are malformed.
-        if pos - self._cursor_position > 1:
-            self._logger.warning(f"{self._line_number} is ignored.")
+
+        # The we can't step up we step more than one.
+        # We just log and ignore such lines.
+        if pos - self._previous_position > 1:
+            self._logger.warning(
+                f"Line {self._line_number} is ignored, wrong formatted line."
+            )
             return
 
-        while pos <= self._cursor_position:
+        # The previous position is greater than the current one
+        # then we have to step back
+        while self._previous_position - pos >= 0:
             self._current_dir = self._current_dir.parent
-            pos += 1
-
-        self._cursor_position = pos
+            self._previous_position -= 1
 
         return self._current_dir
 
-    def _validate(self, data: Option):
-        filename = data.get("filename")
-        outdir = data.get("outdir", ".")
-        delimiter = data.get("delimiter", "/")
-        indent = data.get("indent", "\t")
+    def _validate(self, data: typing.Dict):
+        filename = data["filename"]
+        outdir = data["outdir"]
+        delimiter = data["delimiter"]
+        indent = data["indent"]
 
         filename = self._validate_filename(filename)
         outdir = self._validate_outdir(outdir)
@@ -110,7 +152,7 @@ class FileParser:
             filename=filename, outdir=outdir, delimiter=delimiter, indent=indent
         )
 
-    def _validate_filename(filename: typing.Union[str, Path]) -> Path:
+    def _validate_filename(self, filename: typing.Union[str, Path]) -> Path:
         filename = filename if isinstance(filename, Path) else Path(filename)
         filename = absolute(filename)
 
@@ -123,7 +165,10 @@ class FileParser:
 
         return filename
 
-    def _validate_outdir(outdir: typing.Union[str, Path]) -> Path:
+    def _clean_line(self, line: str, delimiter: str, indent: str) -> str:
+        return line.lstrip(indent).rstrip(delimiter)
+
+    def _validate_outdir(self, outdir: typing.Union[str, Path]) -> Path:
         outdir = outdir if isinstance(outdir, Path) else Path(outdir)
         outdir = absolute(outdir)
 
@@ -132,21 +177,18 @@ class FileParser:
 
         return outdir
 
-    def _compute_cursor_position(line: str, indent: str) -> int:
-        match = re.match(r"[" + indent + "]" + "*")
-        return match.span()[1] if match else self._cursor_position
+    def _compute_cursor_position(self, line: str, indent: str) -> int:
+        match = re.match(r"[" + indent + "]" + "*", line)
+        return match.span()[1] if match else self._previous_position
 
     def _config_logger(self):
         c_handler = logging.StreamHandler()
-        c_formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
+        c_formatter = logging.Formatter("%(name)s [%(levelname)s] - %(message)s")
 
-        c_handler.setLevel(self.logger_level)
-        c_handler.addFormatter(c_formatter)
+        c_handler.setLevel(self._logger_level)
+        c_handler.setFormatter(c_formatter)
         self._logger.addHandler(c_handler)
 
-    @classmethod
     @property
     def name(self):
         return self.__class__.__name__
